105991af280429ac5a196bea775f31fe1896f320
Luis A. Salazar Leon <salazarleon.luisangel2005@gmail.com>
2026-09-12 10:29:12 -0500

    subiendo todos los archivos del proyecto

---
+++ src/main/java/com/pe/nexuslogix/NexuslogixApplication.java
@@ -0,0 +1,13 @@
+package com.pe.nexuslogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuslogixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuslogixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/nexuslogix/NexuslogixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.nexuslogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuslogixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
